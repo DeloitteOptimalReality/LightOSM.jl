3dfc76dee66567f0a7421029ac7d2400dbe69a49
Mal Miller <mmiller212365@gmail.com>
2021-08-02 15:44:18 +0100

    Fix nearest_node and add tests
    - Remove abstract typing of output
    - Fix use of to_cartesian
    - Add basic tests
    - Fix Vector{<:Vector{<:AbstractFloat}} method

--- src/nearest_node.jl
+++ src/nearest_node.jl
@@ -12,13 +12,13 @@ Finds nearest nodes from a vector of GeoLocations using a `NearestNeighbors.jl` 
 - `n_neighbours::Integer`: Number of neighbours to query for each point.
 
 # Return
-- `Tuple{Vector{Vector{Integer}}, Vector{Vector{AbstractFloat}}}`: Tuple of neighbours and straight line euclidean distances from each point `([[neighbours]...], [[dists]...])`.
+- Tuple of neighbours and straight line euclidean distances from each point `([[neighbours]...], [[dists]...])`.
 """
-function nearest_node(g::OSMGraph,
+function nearest_node(g::OSMGraph{U, T, W},
                       points::Vector{GeoLocation},
                       n_neighbours::Integer=1
-                      )::Tuple{Vector{Vector{Integer}},Vector{Vector{AbstractFloat}}}
-    cartesian_locations = transpose(LightOSM.to_cartesian(points))
+                      )::Tuple{Vector{Vector{T}},Vector{Vector{Float64}}} where {U, T, W}
+    cartesian_locations = LightOSM.to_cartesian(points)
     idxs, dists = knn(g.kdtree, cartesian_locations, n_neighbours, true)
     neighbours = [[g.index_to_node[j] for j in i] for i in idxs]
     dists = [d for d in dists]
@@ -26,16 +26,16 @@ function nearest_node(g::OSMGraph,
 end
 
 nearest_node(g::OSMGraph, point::GeoLocation, n_neighbours::Integer=1) = nearest_node(g, [point], n_neighbours)
-nearest_node(g::OSMGraph, points::Vector{Vector{<:AbstractFloat}}, n_neighbours::Integer=1) = nearest_node(g, GeoLocation(points), n_neighbours)
+nearest_node(g::OSMGraph, points::Vector{<:Vector{<:AbstractFloat}}, n_neighbours::Integer=1) = nearest_node(g, GeoLocation(points), n_neighbours)
 nearest_node(g::OSMGraph, point::Vector{<:AbstractFloat}, n_neighbours::Integer=1) = nearest_node(g, GeoLocation(point), n_neighbours)
 
 """
 Finds nearest nodes from a vector of node ids, the query node id is excluded from neighbours.
 """
-function nearest_node(g::OSMGraph,
+function nearest_node(g::OSMGraph{U, T, W},
                       nodes::Vector{<:Integer},
                       n_neighbours::Integer=1
-                      )::Tuple{Vector{Vector{Integer}},Vector{Vector{AbstractFloat}}}
+                      )::Tuple{Vector{Vector{T}},Vector{Vector{Float64}}} where {U, T, W}
     locations = [g.nodes[n].location for n in nodes]
     n_neighbours += 1 # Closest node is always the input node itself, exclude self from result
     neighbours, dists = nearest_node(g, locations, n_neighbours)
