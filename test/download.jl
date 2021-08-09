ce50be68e231680d33e27220662aa43fc80be121
Mal Miller <>
2021-08-09 17:51:41 +0100

    Fix JSON loading

--- test/download.jl
+++ test/download.jl
@@ -5,3 +5,41 @@
     @test LightOSM.osm_network_downloader(:polygon) == LightOSM.osm_network_from_polygon
     @test_throws ArgumentError LightOSM.osm_network_downloader(:doesnt_exist)
 end
+
+@testset "Download end to end formats" begin
+    # JSON
+    filename = "melbourne_1k.json"
+    data = download_osm_network(:point,
+                                radius=0.5,
+                                point=GeoLocation(-37.8136, 144.9631),
+                                network_type=:drive,
+                                download_format=:json,
+                                save_to_file_location=filename);
+    @test isfile(filename)
+    g = graph_from_file(filename) # Check it doesn't error
+    rm(filename)
+    
+    # XML
+    filename = "melbourne_1k.xml"
+    data = download_osm_network(:point,
+                                radius=0.5,
+                                point=GeoLocation(-37.8136, 144.9631),
+                                network_type=:drive,
+                                download_format=:xml,
+                                save_to_file_location=filename);
+    @test isfile(filename)
+    g = graph_from_file(filename) # Check it doesn't error
+    rm(filename)
+
+    # OSM
+    filename = "melbourne_1k.osm"
+    data = download_osm_network(:point,
+                                radius=0.5,
+                                point=GeoLocation(-37.8136, 144.9631),
+                                network_type=:drive,
+                                download_format=:osm,
+                                save_to_file_location=filename);
+    @test isfile(filename)
+    g = graph_from_file(filename) # Check it doesn't error
+    rm(filename)
+end
