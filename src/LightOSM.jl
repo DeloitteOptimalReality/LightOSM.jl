8a495a70904699426198d77e2612ebe86dd40564
Laurenz <laurenz.rasche@gmail.com>
2021-12-20 18:10:28 +0100

    simplify_graph() - remove interstitial nodes from graph

--- src/LightOSM.jl
+++ src/LightOSM.jl
@@ -5,7 +5,7 @@ using DataStructures: DefaultDict, OrderedDict, MutableLinkedList
 using QuickHeaps: BinaryHeap, FastMin
 using Statistics: mean
 using SparseArrays: SparseMatrixCSC, sparse, findnz
-using Graphs: AbstractGraph, DiGraph, nv, outneighbors, weakly_connected_components, vertices
+using Graphs: AbstractGraph, DiGraph, nv, outneighbors, weakly_connected_components, vertices, all_neighbors, indegree, outdegree, add_edge!
 using StaticGraphs: StaticDiGraph
 using SimpleWeightedGraphs: SimpleWeightedDiGraph
 using MetaGraphs: MetaDiGraph, set_prop!
@@ -15,6 +15,8 @@ using JSON
 using LightXML
 using StaticArrays
 using SpatialIndexing
+using ArchGDAL: IGeometry, createlinestring, createpoint
+using DataFrames
 
 export GeoLocation,
        OSMGraph,
@@ -67,7 +69,8 @@ export index_to_node_id,
        set_dijkstra_state_with_index!,
        set_dijkstra_state_with_node_id!,
        maxspeed_from_index,
-       maxspeed_from_node_id
+       maxspeed_from_node_id,
+       simplify_graph
 
 include("types.jl")
 include("constants.jl")
@@ -83,5 +86,6 @@ include("nearest_node.jl")
 include("nearest_way.jl")
 include("buildings.jl")
 include("subgraph.jl")
+include("simplification.jl")
 
 end # module
---
+++ src/simplification.jl
@@ -0,0 +1,96 @@
+
+#adapted from osmnx: https://github.com/gboeing/osmnx/blob/main/osmnx/simplification.py
+
+"""
+Predicate wether v is an edge endpoint in the simplified version of g
+"""
+function is_endpoint(g::AbstractGraph, v)
+    neighbors = all_neighbors(g, v)
+    if v in neighbors # has self loop
+        return true
+    elseif outdegree(g, v) == 0 || indegree(g, v) == 0 # sink or source
+        return true
+    elseif length(neighbors) != 2 || indegree(g, v) != outdegree(g, v) # change to one way
+        return true
+    end
+    return false
+end
+
+"""
+iterator over all endpoints in g
+"""
+endpoints(g::AbstractGraph) = (v for v in vertices(g) if is_endpoint(g, v))
+
+"""
+iterator over all paths in g which can be contracted
+"""
+function paths_to_reduce(g::AbstractGraph)
+    (path_to_endpoint(g, (u, v)) for u in endpoints(g) for v in outneighbors(g, u))
+end
+
+"""
+path to the next endpoint starting in edge (ep, ep_succ)
+"""
+function path_to_endpoint(g::AbstractGraph, (ep, ep_succ)::Tuple{T,T}) where {T<:Integer}
+    path = [ep, ep_succ]
+    head = ep_succ
+    # ep_succ not in endpoints -> has 2 neighbors and degree 2 or 4
+    while !is_endpoint(g, head)
+        neighbors = [n for n in outneighbors(g, head) if n != path[end-1]]
+        @assert length(neighbors) == 1 "found unmarked endpoint!"
+        head, = neighbors
+        push!(path, head)
+        (head == ep) && return path # self loop
+    end
+    return path
+end
+
+"""
+Build a new graph which simplifies the topology of osmg.graph.
+The resulting graph only contains intersections and dead ends from the original graph.
+The geometry of the contracted nodes is kept in the edge_gdf DataFrame
+"""
+function simplify_graph(osmg::OSMGraph)
+    g = osmg.graph
+    relevant_nodes = collect(endpoints(g))
+    n = length(relevant_nodes)
+    (n == nv(g)) && return g # nothing to simplify here
+
+
+    G_simplified = DiGraph(n) 
+    weights = similar(osmg.weights, (n, n))
+    edge_gdf = DataFrame(
+        u = Int[],
+        v = Int[],
+        key = Int[],
+        weight = Vector{eltype(osmg.weights)}(),
+        geom = IGeometry[],
+    )
+    node_gdf = DataFrame(id = Int[], geom = IGeometry[])
+
+
+    index_mapping = Dict{Int,Int}()
+    for (new_i, old_i) in enumerate(relevant_nodes)
+        index_mapping[old_i] = new_i
+        geo = createpoint(osmg.node_coordinates[old_i])
+        push!(node_gdf, (new_i, geo))
+    end
+
+    for path in paths_to_reduce(g)
+        u = index_mapping[first(path)]
+        v = index_mapping[last(path)]
+        edge_weight = sum((osmg.weights[i, i+1] for i in 1:length(path)-1))
+        geo = createlinestring(osmg.node_coordinates[path])
+
+        if add_edge!(G_simplified, (u, v))
+            key = 0
+            weights[u, v] = edge_weight
+        else # parallel edge
+            key = sum((edge_gdf.u .== u) .& (edge_gdf.v .== v))
+            weights[u, v] = min(edge_weight, weights[u, v])
+        end
+        push!(edge_gdf, (u, v, key, edge_weight, geo))
+    end
+
+    return G_simplified, weights, node_gdf, edge_gdf
+end
