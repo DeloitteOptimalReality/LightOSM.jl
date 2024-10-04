4b64eda5f8d8f2b4f73eb251e787eabe3f91c3b3
rush42 <laurenz.rasche@gmail.com>
2024-10-04 14:34:55 +0200

    generalize plot recipes

--- src/plotrecipes.jl
+++ src/plotrecipes.jl
@@ -1,10 +1,8 @@
-function aspect_ratio(g::OSMGraph) 
+function aspect_ratio(g::AbstractOSMGraph) 
     max_y, min_y = extrema(first, g.node_coordinates)
     mid_y = (max_y + min_y)/2
     return 1/cos(mid_y * pi/180)
 end
-aspect_ratio(sg::SimplifiedOSMGraph) = aspect_ratio(sg.parent)
-
 
 RecipesBase.@recipe function f(g::AbstractOSMGraph)
     color --> :black
