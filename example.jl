6d40cc70fde1083f63f8d0ed703e0cc1c2304dce
rush42 <laurenz.rasche@gmail.com>
2024-10-06 18:10:52 +0200

    update example: test toplogy with `shortest_path` on random `source` and `destination` samples

--- example.jl
+++ example.jl
@@ -1,23 +1,38 @@
 using LightOSM, Plots, GeoInterfaceRecipes
+using StatsBase: sample
 
 g = graph_from_download(
     :place_name, 
     place_name="tiergarten, berlin germany",
-    network_type=:bike
+    network_type=:drive
 )
 sg = simplify_graph(g)
 
-# check for missing edges
-plot(g, color=:red, linewidth=0.8, size=(800, 800))
-plot!(sg, linewidth=1.1, color=:black)
-savefig("edge_validation")
+# Set plot size
+size = (1920, 1080)
 
-# show original nodes
-plot(sg)
-plot!(node_gdf(g).geom, color=:red, markersize=2.2)
+# Show original nodes
+plot(g; size)
 savefig("original_nodes")
 
-# show relevant nodes
-plot(sg)
-plot!(node_gdf(sg).geom, color=:green, markersize=2.2)
+# Show relevant nodes
+plot(sg; size)
 savefig("relevant_nodes")
+
+
+
+osm_ids = sample(collect(values(sg.nodes)), 200)
+for source in osm_ids
+    for target in osm_ids
+        path = shortest_path(g, source, target)
+        path_simplified = shortest_path(sg, source, target)
+        if isnothing(path) || isnothing(path_simplified)
+            continue
+        end
+        path_length = total_path_weight(g, path)
+        path_simplified_length = total_path_weight(sg, path_simplified)
+        if !isapprox(path_length, path_simplified_length)
+            error("Path from $source to $target is $path_length in the original graph and $path_simplified_length in the simplified graph")
+        end
+    end
+end
